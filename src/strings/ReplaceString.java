689aade7df5f8380d46ce34165bc7bb3b89e5847
sanat-biswas <sanatbiswas786@gmail.com>
2024-07-20 22:08:54 +0530

    add: replace string using the single parameter

--- src/strings/ReplaceString.java
+++ src/strings/ReplaceString.java
@@ -4,6 +4,8 @@ public class ReplaceString {
     public static void main(String[] args) {
         String s = "baccad";
         replaceString("", s);
+
+        System.out.println(replaceStringWithRecursion(s));
     }
 
     static void replaceString(String processed, String unprocessed) {
@@ -15,11 +17,20 @@ public class ReplaceString {
         char ch = unprocessed.charAt(0);
         if(ch == 'a') {
             replaceString(processed, unprocessed.substring(1));
-//            System.out.println(processed + " pr");
         } else {
             replaceString(processed + ch, unprocessed.substring(1));
-
-//            System.out.println(processed + " processed");
         }
+    }
+
+    static String replaceStringWithRecursion(String unprocessed) {
+        if(unprocessed.isEmpty()) {
+            return "";
+        }
+
+        char ch = unprocessed.charAt(0);
+
+        if(ch == 'a')
+            return replaceStringWithRecursion(unprocessed.substring(1));
+        return ch + replaceStringWithRecursion(unprocessed.substring(1));
     }
 }
